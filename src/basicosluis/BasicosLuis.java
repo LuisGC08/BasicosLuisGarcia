4ee0491ce179636ec12f38e75fd89311dd7727fd
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2019-12-02 11:19:30 +0100

    en la rama numericos metemos codigo

--- src/basicosluis/BasicosLuis.java
+++ src/basicosluis/BasicosLuis.java
@@ -10,12 +10,46 @@ package basicosluis;
  * @author usuario
  */
 public class BasicosLuis {
+  public static void cadenas() {
+    	 String cadena;
+         cadena="HolaMundo";
+         System.out.println(cadena);
+     }
+	public static void logicos() {
+		boolean logic=true;
+		if(logic=true) {
+			System.out.println("Verdadero");
+		}
+		else {
+			System.out.println("false");
+		}
 
-    /**
-     * @param args the command line arguments
-     */
+	}
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       // TODO Auto-generated method stub
+	 numericos();  // Llamada al método
+	 cadenas();
     }
     
 }
