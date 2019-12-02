7ebe7cde4ff192604616b18a510f24cb4a1c865e
Luis Garcia Clavel <luisgarciac2esob@gmail.com>
2019-12-02 11:22:03 +0100

    en la rama cadenas

--- src/basicosluis/BasicosLuis.java
+++ src/basicosluis/BasicosLuis.java
@@ -10,46 +10,23 @@ package basicosluis;
  * @author usuario
  */
 public class BasicosLuis {
-  public static void cadenas() {
-    	 String cadena;
-         cadena="HolaMundo";
-         System.out.println(cadena);
-     }
-	public static void logicos() {
-		boolean logic=true;
-		if(logic=true) {
-			System.out.println("Verdadero");
-		}
-		else {
-			System.out.println("false");
-		}
+   public static void cadenas() {
 
-	}
-    public static void numericos() {
+      String cadena;
+      cadena="HolaMundo";
 
-        //Estos son los tipos básicos
-        byte elMasPeque;
-        short chiquitin;
-        int entero;
-        long duplicaEntero;
-        float grande;
-        double doble;
-
-        //Ejemplos de asignación de valores a las variables de tipos básicos
-        entero = 10;
-        doble = 5.6;
+        
 
         //Mostrar en pantalla el contendido de las variables de tipos básicos.
-        System.err.println("Valor entero");
-        System.out.println(entero);
-        System.out.println("Valor doble");
-        System.out.println(doble);
+        
+        
+        System.out.println(cadena);
+        
     }
 
     public static void main(String[] args) {
        // TODO Auto-generated method stub
-	 numericos();  // Llamada al método
-	 cadenas();
+	 
     }
     
 }
